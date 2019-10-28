2cc92e2c97c18a1997891cf40f45edbbb3907b4c
Jason Kim <jason.kim.jiho@gmail.com>
2019-10-28 18:55:57 -0400

    init commit - code provided by Edwin

---
+++ src/main/scala/SparkJob/CsvJob.scala
@@ -0,0 +1,30 @@
+package SparkJob
+
+import SparkJob.Domain.SparkParams
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.lit
+
+object CsvJob {
+    def run(sparkParams: SparkParams) {
+        // create sparkSession
+        val spark = SparkSession
+                    .builder
+                    .appName(sparkParams.parser)
+                    .getOrCreate()
+
+        // Homework: change the way we use the option to inOption
+        val data = spark.read.option("header", "false").option("delimiter", ",").csv(sparkParams.inPath)
+
+        // transformation logic below
+        val resultDF = data.withColumn("source", lit("wcd"))
+
+        resultDF.write
+        .partitionBy(sparkParams.partitionColumn)
+        .options(sparkParams.outOptions)
+        .format(sparkParams.outFormat)
+        .mode(sparkParams.saveMode)
+        .save(sparkParams.outPath)
+    }
+
+
+}
